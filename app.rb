65781a52cd6cf911854bafe6c4429bf792c0bdd1
Kevin Pruett <pruett.kevin@gmail.com>
2013-10-30 18:31:39 -0400

    add routes

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class BisonAPI < Sinatra::Base
   helpers Sinatra::JSON
 
   configure do
-    set :DB, Mongo::Connection.new.db("bison_db", :pool_size => 5, :timeout => 5)
+    set :DB, Mongo::Connection.new.db("bison_db", pool_size: 5, timeout: 5)
     set :collection_name, "campaigns"
   end
 
@@ -18,7 +18,7 @@ class BisonAPI < Sinatra::Base
 
     def document_by_id id
       id = object_id(id) if String === id
-      settings.DB.collection(settings.collection_name).find_one(:_id => id)
+      settings.DB.collection(settings.collection_name).find_one(_id: id)
     end
   end
 
@@ -34,13 +34,18 @@ class BisonAPI < Sinatra::Base
     json document_by_id(params[:id])
   end
 
-  get '/api/campaigns/location/:location' do
-    json settings.DB.collection(settings.collection_name).find(:location => params[:location]).to_a
+  get '/api/campaigns/location/:location/?' do
+    json settings.DB.collection(settings.collection_name).find(location: params[:location]).to_a
   end
 
   post '/api/campaigns' do
     data = JSON.parse(request.body.read)
     oid = settings.DB.collection(settings.collection_name).insert(data)
     json id: oid.to_s
+  end
+
+  delete '/api/campaigns/remove/:id/?' do
+    settings.DB.collection(settings.collection_name).remove(_id: object_id(params[:id]))
+    json success: true
   end
 end
